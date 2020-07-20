860abd1cea7926094331ae9b2cdd22041963052d
Deepu19g <62106634+Deepu19g@users.noreply.github.com>
2020-07-20 07:10:06 -0700

    Add files via upload

---
+++ necro.js
@@ -0,0 +1,8 @@
+
+     function onclickmenu(){
+        document.getElementById("menu").classList.toggle("change");
+        document.getElementById("na").classList.toggle("change");
+       
+        document.getElementById("menu-bg").classList.toggle("change-bg");
+      
+      }
