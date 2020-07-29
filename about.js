bb9b88b5822ba2e8cd19f9b69fe6d7e2ddb8e3f2
Deepu19g <deepakmathew7d@gmail.com>
2020-07-29 07:45:36 -0700

    tenth commit

--- about.js
+++ about.js
@@ -1,7 +1,7 @@
 
 var i = 0;
 
-var txt = 'I am Deepak Mathew, a first year student at Govt.Model Engineering college, majoring in Computer Engineering and currently staying in Kochi.Ever since I was a kid, Ive wanted to work in the field of Computers. I love reading and spent most of the time spending in front of my laptop. One of my dreams was to master in it and become one of the top Software Engineers in the world and now I am working towards it. I do work for what I dream. I had always showed keen interest in this field. I have developed skills in different computer languages like C, C++, Python, Dart etc..With my experience in computer programming , I am able to create different projects and help others with that knowledge. I try different things in my idle hours and try to make best use of it. Apart from this, I love playing and watching both football and cricket.'; 
+var txt = 'I am Deepak Mathew, a first year student at Govt.Model Engineering college, majoring in Computer Engineering and currently staying in Kottayam.i didnt have much interest in computer science but in time i started to like it.i started to learn html during lockdown days,and after some time i thought about doing a website for practice. I spent most of the time spending in front of my laptop. i am interested in learning more in this field. I have learned different computer languages like C, C++, Python along with html,css,javascript etc.. I try to watch webseries and read a bit in my free time'; 
 
 
 
